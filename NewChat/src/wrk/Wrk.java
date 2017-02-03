92ac035d1294885f83594a6f83687a253b0fdd03
Nathan Canzali <nathan.canzali@gmail.com>
2017-02-03 09:17:45 +0100

    OpenPort in wrk

--- NewChat/src/wrk/Wrk.java
+++ NewChat/src/wrk/Wrk.java
@@ -5,10 +5,26 @@
  */
 package wrk;
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
 /**
  *
  * @author Nathan
  */
 public class Wrk {
+    public void ouvrirPort(){
+        this.serialPort = new SerialPort("COM1");
+        try {            
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
+        } catch (SerialPortException ex) {
+           System.out.println(ex);
+        }
+    }
     
+    private SerialPort serialPort;
 }
